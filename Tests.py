8c3bfa9fdfca53a7b60ca6738c31d0adba86d39d
GPReece22 <56441624+GPReece22@users.noreply.github.com>
2019-10-31 22:28:45 +0000

    Delete Tests.py

--- Tests.py
+++
@@ -1,8 +0,0 @@
-scores = []
-
-for result in range(4):
-    scores.append((float(input("Please enter score " + str(result+1)))))
-
-print("the highest score was ", max(scores))
-print("the lowest score was ", min(scores))
-print("the average score was ", sum(scores)/len(scores))
